527fb7d0dc06b48bc978ab7e61fecf0b68b217f9
Rachita01 <rachitasinghal6@gmail.com>
2021-05-15 15:25:43 +0530

    Delete and update todo

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {addTodo} from '../redux/actions';
+import {v1 as uuid} from 'uuid';
+import { useDispatch } from 'react-redux';
 
 function TodoInput(){
-    
+    let[name,setName] = useState();
+    let dispatch = useDispatch();
     return(
         <div>
           <div className="col mx-4">
-            <input className="input-box" type="text"/>
-            <Button variant="primary" className="col ms-2">Add</Button>
+            <input className="input-box" type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+            <Button variant="primary" className="col ms-2" 
+            onClick={() => {dispatch(addTodo(
+              { id: uuid(),
+                name:name
+                }
+                ))
+                setName('');
+            }}>Add</Button>
           </div>
         </div>
     )
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useDispatch } from 'react-redux';
-import { deleteTodo } from '../redux/actions';
+import { deleteTodo, updateTodo } from '../redux/actions';
 
 function TodoItem({todo}){
     let dispatch = useDispatch();
+    const [editable,setEditable] = useState(false);
+    const [name,setName] = useState(todo.name);
     return(
         <div>
           <div className="row ms-2">
-            <div className="col ms-2">#{todo.id}</div>
-            <div className="col ms-2">{todo.name}</div>
-            <Button variant="primary m-2" className="col">Edit</Button>
+            <div className="col ms-2">#{todo.id.length > 1? todo.id[2]:todo.id}</div>
+            <div className="col ms-2"> {editable? <input type="text" className="form-control" onChange={(e) => setName(e.target.value)} value={name}/> : <h4>{todo.name}</h4>}</div>
+            <Button variant="primary m-2" className="col"
+             onClick={() => {
+               dispatch(updateTodo({
+                 ...todo,
+                 name:name
+               }))
+             if(editable){
+               setName(todo.name);
+               console.log(todo.name);
+             }
+             setEditable(!editable)}}>
+             {editable?"Update":"Edit"}
+            </Button>
             <Button variant="danger  m-2" className="col" onClick={()=> dispatch(deleteTodo(todo.id))}>Delete</Button>
           </div>
          
